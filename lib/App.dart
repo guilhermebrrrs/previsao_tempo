e74fb235d5e823df0134f1de4d28834a491c2007
guilhermebrrrs <guilhermebrrrs@hotmail.com>
2021-11-03 21:44:09 -0300

    chore: initiate app structure folder and home

---
+++ lib/App.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:previsao_tempo/screens/Home.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+      title: "Previsão do Tempo",
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:previsao_tempo/App.dart';
+
+void main() => runApp(App());
